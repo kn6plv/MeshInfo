3f5e12b893cb7353d0a353128f2175d7b48b80a4
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2022-10-27 22:31:29 -0700

    Fix paths

--- writeKML.js
+++ writeKML.js
@@ -118,13 +118,13 @@ module.exports = {
       </Point>
     </Placemark>`);
                 
-              Object.values(node.link_info || {}).forEach(link => {
+                Object.values(node.link_info || {}).forEach(link => {
                     const host1 = node.node.toLowerCase();
                     const host2 = link.hostname.toLowerCase();
                     const k1 = `${host1}/${host2}`;
                     const k2 = `${host2}/${host1}`;
                     if (!links[k1] && !links[k2] && (link.linkType === "RF" || (link.linkType === "DTD" && update.groups[host1] !== update.groups[host2]))) {
-                        const onode = update.nodes.find(n => n.node == host2);
+                        const onode = update.nodes.find(n => n.node.toLowerCase() == host2);
                         if (onode && onode.lat && onode.lon) {
                             kmlpaths.push(`<Placemark><name>${node.node} to ${onode.node}</name><LineString><altitudeMode>relativeToGround</altitudeMode><coordinates>${node.lon},${node.lat},10 ${onode.lon},${onode.lat},10</coordinates></LineString></Placemark>`);
                         }

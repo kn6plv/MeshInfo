fe9bb0fc9ecd7fcf5adde2b7f8be16442707c79d
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2025-03-23 16:38:02 -0700

    Split out tunnel types in CSV

--- writeCSV.js
+++ writeCSV.js
@@ -10,7 +10,7 @@ module.exports = {
 
     write(update, filename) {
         const csvtable = [
-            'node,wlan_ip,last_seen,uptime,loadavg,hardware,model,firmware_version,ssid,channel,chanbw,tunnel_installed,active_tunnel_count,lat,lon,wifi_mac_address,api_version,board_id,firmware_mfg,grid_square,lan_ip,services,location_fix,lqm'
+            'node,wlan_ip,last_seen,uptime,loadavg,hardware,model,firmware_version,ssid,channel,chanbw,tunnel_installed,active_tunnel_count,legacy_tunnel_count,wireguard_tunnel_count,lat,lon,wifi_mac_address,api_version,board_id,firmware_mfg,grid_square,lan_ip,services,location_fix,lqm'
         ];
 
         update.nodes.forEach(node => {
@@ -21,7 +21,7 @@ module.exports = {
                     `"${node.node_details.hardware}",` +
                     `"${node.node_details.model}",${node.node_details.firmware_version},` +
                     `${node.meshrf && node.meshrf.ssid || 'None'},${node.meshrf && node.meshrf.channel || 'None'},${node.meshrf && node.meshrf.chanbw || 'None'},` +
-                    `${node.tunnels.tunnel_installed || true},${node.tunnels.active_tunnel_count},${node.lat || '"Not Available"'},${node.lon || '"Not Available"'},${(node.interfaces.find(i => i.ip && (i.name === 'wlan0' || i.name === 'wlan1' || i.name === 'eth1.3975')) || {}).mac || 'Unknown'},` +
+                    `${node.tunnels.tunnel_installed || true},${node.tunnels.active_tunnel_count},${"legacy_tunnel_count" in node.tunnels ? node.tunnels.legacy_tunnel_count : ''},${"wireguard_tunnel_count" in node.tunnels ? node.tunnels.wireguard_tunnel_count : ''},${node.lat || '"Not Available"'},${node.lon || '"Not Available"'},${(node.interfaces.find(i => i.ip && (i.name === 'wlan0' || i.name === 'wlan1' || i.name === 'eth1.3975')) || {}).mac || 'Unknown'},` +
                     `${node.api_version},${node.node_details.board_id},${node.node_details.firmware_mfg},` +
                     `${node.grid_square || '"Not Available"'},${(node.interfaces.find(i => i.name === 'br-lan') || {}).ip || '"Not Available"'},` +
                     `"a:${(node.services_local || []).length}:{${(node.services_local || []).map((s, i) => 'i:' + i + ';a:3:{s:4:""name"";s:' + s.name.length + ':""' + s.name + '"";s:8:""protocol"";s:' + s.protocol.length + ':""' + s.protocol + '"";s:4:""link"";s:' + s.link.length + ':""' + s.link + '"";}').join('')}}",0,` +
